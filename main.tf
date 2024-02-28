cf6955e312490a7a474e51778cc62e7b3004d873
Raghavender Reddy Lonka <96302103+Raghavender-lonka@users.noreply.github.com>
2024-02-28 16:08:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "newrelic_workload" "foo" {
     name = "Synethic Workload" #(Required) The workload's name.
     account_id = 3627436 #(Required) The New Relic account ID where you want to create the workload.
 
-    entity_guids = [for id in var.guids : id] #(Optional) A list of entity GUIDs manually assigned to this workload.
+    #entity_guids = [for id in var.guids : id] #(Optional) A list of entity GUIDs manually assigned to this workload.
 
     # entity_search_query {
     #     query = "name like '%Example application%'"
